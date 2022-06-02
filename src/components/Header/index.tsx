a8bf5a903c6686c2cfc87db16a9122759b928c3a
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-06-01 23:06:05 -0300

    feat(header): adding responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
-import { Center } from '@chakra-ui/react';
+import { Center, useBreakpointValue } from '@chakra-ui/react';
 import Image from 'next/image';
 
 export function Header() {
+  const isWide = useBreakpointValue({
+    base: false,
+    sm: true,
+  });
+
+  const dimensions = {
+    width: isWide ? 184 : 80,
+    height: isWide ? 46 : 20,
+  };
+
   return (
-    <Center py="4">
-      <Image src="/logo.svg" width={80} height={20} alt="World trip" />
+    <Center py={['4', '7']}>
+      <Image src="/logo.svg" alt="World trip" {...dimensions} />
     </Center>
   );
 }
