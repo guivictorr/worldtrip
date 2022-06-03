82d5ff291e7483bf05bc6c946ec9dfb0c16202e1
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-06-03 00:19:56 -0300

    feat(header): adding back button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
-import { Center, useBreakpointValue } from '@chakra-ui/react';
+import {
+  Center,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import { IoIosArrowBack } from 'react-icons/io';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export function Header() {
   const isWide = useBreakpointValue({
@@ -7,13 +17,29 @@ export function Header() {
     sm: true,
   });
 
+  const router = useRouter();
+
   const dimensions = {
     width: isWide ? 184 : 80,
     height: isWide ? 46 : 20,
   };
 
   return (
-    <Center py={['4', '7']}>
+    <Center py={['4', '7']} px={['4', '8']} position="relative">
+      {router.pathname !== '/' && (
+        <Link href="/" passHref>
+          <IconButton
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            variant="unstyled"
+            icon={<Icon as={IoIosArrowBack} fontSize="lg" />}
+            aria-label="Voltar"
+            position="absolute"
+            left="6"
+          />
+        </Link>
+      )}
       <Image src="/logo.svg" alt="World trip" {...dimensions} />
     </Center>
   );
